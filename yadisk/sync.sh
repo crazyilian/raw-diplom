a9571e0804744cda66862574f963a15186d714b1
Ilian Andrianov <ilian.and.04@gmail.com>
2026-04-05 21:58:38 +0300

    token in env

--- yadisk/sync.sh
+++ yadisk/sync.sh
@@ -1,8 +1,10 @@
 # !/usr/bin/sh
 
+source $(dirname $0)/SECRETS.env
+
 python ./yadisk/yadisk_${1}.py \
     --local ./runs/$2 \
     --remote-root "disk:/raw-diplom/runs/$2" \
-    --token "XXX" \
+    --token $TOKEN \
     --overwrite-different-md5
 
