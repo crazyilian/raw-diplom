fa56bd7f41e42bf52c9aea7a6de6ad63058d2d60
Ilian Andrianov <ilian.and.04@gmail.com>
2026-05-05 22:44:43 +0300

    fix

--- runners/debug-TCN-light-forecasting.py
+++ runners/debug-TCN-light-forecasting.py
@@ -132,4 +132,4 @@ print("planned runs after slice:", len(sweep_configs))
 
 ##### run experiments #####
 
-run_experiments(sweep_configs, skip_existing=True, dry_run=True)
+run_experiments(sweep_configs, skip_existing=True, dry_run=False)
